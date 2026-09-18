8d04fbe37040815b3125acf9651244327a823382
Jyotibrat <jyotibratb@gmail.com>
2026-09-19 02:57:52 +0530

    fix: fixed a scss bug related to /docs pg

--- src/pages/DocsPage.jsx
+++ src/pages/DocsPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./AboutPage.scss"; // Using AboutPage styles temporarily or creating a generic one
 
 const DocsPage = () => {
   useEffect(() => {
